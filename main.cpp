63d91fb34da701db7b6c7ec931e9a43211c9c1e1
Nikola <nikola.dey.georgiev@gmail.com>
2023-12-28 12:31:53 +0200

    dblIntAdd

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 
+//  perform an addition of two unsigned integers (a and c) with a carry (b) and handle overflow by incrementing the carry.
+void dblIntAdd(unsigned int& a, unsigned int& b, unsigned int c) {
+    if (a > 0xffffffffu - c) {
+        ++b;
+    }
+    a += c;
+}
+
+
 int main(){
 
     return 0;
