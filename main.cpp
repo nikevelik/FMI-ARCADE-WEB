e4f513e40b88a235702a54346dd9150989a152b9
Nikola <nikola.dey.georgiev@gmail.com>
2023-12-29 13:14:05 +0200

    read file

--- main.cpp
+++ main.cpp
@@ -152,7 +152,26 @@ bool compareHashes(const char* hash1, const char* hash2) {
     return 1;
 }
 
+unsigned int MAX_FILESIZE_BYTES = 1024;
 
+bool readFile(const char* filename, char* dest) {
+    if(!dest || !filename){
+        return 0;
+    }
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open()) {
+        return false;
+    }
+    std::streamsize fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+    if (fileSize > MAX_FILESIZE_BYTES) {
+        return false;
+    }
+    file.read(dest, fileSize);
+    dest[fileSize] = '\0';
+    file.close();
+    return true;
+}
 
 int main(){
 
@@ -182,6 +201,11 @@ int main(){
      ///   std::cout << "are not the same.\n";
     ///}
 
+    ///const char* filename = "1KB.txt";
+    ///char fileContent[1024+1] = "";
+    ///if (readFile(filename, fileContent)) {
+    ///    std::cout << "File content:\n" << fileContent << std::endl;
+    ///}
 
     return 0;
 }
